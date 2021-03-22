8b068745d7d1bfd3628ee9f09a7c4047000cab3e
zainnadeem <zn.nadeem@gmail.com>
2021-03-22 15:19:47 -0400

    Initial Commit

---
+++ Shared/DetectCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectCheckApp.swift
+//  Shared
+//
+//  Created by Zain Nadeem on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct DetectCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
